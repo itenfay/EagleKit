9a5747f28ab718b64abb639e8c326d11a019202e
Teng Fei <hansen981@126.com>
2024-05-12 21:05:50 +0800

    Add the directory of categories

--- Example/EagleKit/AppDelegate.h
+++ Example/EagleKit/AppDelegate.h
@@ -2,8 +2,8 @@
 //  CXAppDelegate.h
 //  EagleKit
 //
-//  Created by chenxing on 02/21/2024.
-//  Copyright (c) 2024 chenxing. All rights reserved.
+//  Created by Teng Fei on 02/21/2024.
+//  Copyright (c) 2024 Teng Fei. All rights reserved.
 //
 
 @import UIKit;
---
+++ Example/EagleKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EagleKit
+//
+//  Created by Teng Fei on 02/21/2024.
+//  Copyright (c) 2024 Teng Fei. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ViewController : UIViewController
+
+@end
